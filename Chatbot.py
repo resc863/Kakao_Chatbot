0ee23dc92625be55a440c0892656a69ed0c515fd
resc863 <50689611+resc863@users.noreply.github.com>
2019-10-27 18:22:21 +0900

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import urllib
 import json
+import datetime
 
 ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.'
 
@@ -22,10 +23,6 @@ def get_diet(code, ymd, weekday):
                 "&schulKndScCode=04"
                 "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
             )
-        #http://stu.pen.go.kr/ 관할 교육청 주소 확인해주세요.
-        #schulCode= 학교고유코드
-        #schulCrseScCode= 1유치원2초등학교3중학교4고등학교
-        #schulKndScCode= 01유치원02초등학교03중학교04고등학교
 
         #기존 get_html 함수부분을 옮겨왔습니다.
         html = ""
@@ -124,6 +121,63 @@ def meal():
     menu = handler(req, )
 
     return jsonify(menu)
+
+@app.route('/schedule', methods=['POST'])
+def schedule():
+    today = date.today()
+
+    try:
+        officeCode="stu.pen.go.kr" ## 교육청 코드
+        schulCode="C100000521" ## 학교 고유코드
+        schulCrseCode="4" ## 학교 분류코드 (고등학교, 중학교, 초등학교)
+        schulKndScCode="0" + "4" ## 학교 분류코드
+        ay=today.year
+        mm=today.month
+    except:
+        print("""!!ERROR.... 
+                코드 인자 값이 맞지 않거나 값이 이상합니다.
+                제대로 된 코드 값을 입력해주십쇼""")
+        sys.exit(1)
+
+    ##neis web requests
+    URL="https://" + officeCode + "/sts_sci_sf01_001.do"
+    ##params = {'schulCode': 'B100000593', 'schulCrseScCode': '4', 'schulKndScCode' : '04', 'ay' : str(year)} 
+    params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 'schulKndScCode' : str(schulKndScCode), 'ay' : str(ay), 'mm' : str(mm)}
+    response = requests.get(URL, params=params).text
+    data = response[response.find("<tbody>"):response.find("</tbody>")]
+
+    ## re 정규표현식으로 불필요한 데이터를 자르거나 변환
+    regex = re.compile(r'[\n\r\t]')
+    data=regex.sub('',data)
+    rex = re.compile(r'<div class="textL">(.*?)</div>', re.S|re.M)
+    data=rex.findall(data)
+    element = ""
+
+    for dat in data:
+        date=dat[dat.find("<em>"):dat.find("</em>")][4:]
+        alert=dat[dat.find("<strong>"):dat.find("</strong>")][8:]
+        if date == "":
+            continue
+        if alert == "":
+            continue
+    
+        element = element+date+": "+alert+"\n"
+
+    print(element)
+    result1 = {
+        "version": "2.0",
+        "data": {
+            "menu": element
+        }
+    }
+    result2 = {
+        'statusCode': 200,
+        'body': json.dumps(result1),
+        'headers': {
+        'Access-Control-Allow-Origin': '*',
+        },
+    }
+    return jsonify(result2)
 
 
 
