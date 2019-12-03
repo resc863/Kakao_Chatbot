aac45499ccede28f662e503f80cef9e57bebc12a
resc863 <50689611+resc863@users.noreply.github.com>
2019-12-03 17:18:31 +0900

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -13,7 +13,429 @@ app = Flask(__name__)
 
 def get_diet(code, ymd, weekday):
     schMmealScCode = code #int 1조식2중식3석식
+    schYmd = ymd #str 요청할 날짜 yyyy.mm.ddfrom flask import Flask, request, jsonify
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib, requests, re
+import json
+import datetime
+import time
+import random
+from datetime import date
+
+ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.'
+
+
+app = Flask(__name__)
+
+def dating(self) :
+  self = str(self)
+  y = self[:4]
+  m = self[4:6]
+  d = self[6:8]
+  if len(self) == 10 :
+    h = self[8:10]
+    return y+"년 "+m+"월 "+d+"일 "+h+"시"
+  return y+"년 "+m+"월 "+d+"일"
+
+def get_micro(ar) : #대기질 정보 파싱 함수
+  area = {'광복동':'221112', '초량동':'221131', '태종대':'221141', '전포동':'221152', '온천동':'221162','명장동':'221163', '대연동':'221172', '학장동':'221181', '덕천동':'221182', '청룡동':'221191', '좌동':'221192', '장림동':'221202', '대저동':'221211', '녹산동':'221212', '연산동':'221221', '기장음':'221231', '용수리':'221233', '수정동':'221241', '부곡동':'221251', '광안동':'221271', '대신동':'221281'}
+  #측정기 지역 및 지역 번호
+  key = 'ExhrDuBJZ28eMHPRIyFToDuqoT1Lx3ViPoI3uKVLS%2FyucnbaLbQISs4%2FSJWf0AzAV1gkbbtZK5GWvO9clF%2B1aQ%3D%3D'
+  #발급받은 공공 인증키
+  url = 'http://apis.data.go.kr/6260000/AirQualityInfoService/getAirQualityInfoClassifiedByStation?serviceKey='+key+'&numOfRows=1&resultType=json&areaIndex='
+  #파싱할 json의 url
+
+  ar = str(ar)
+  #지역 입력
+  p=""
+  if ar == "주소" :
+    for i in area.keys() :
+      p = p + i+" : '"+url+area[i]+"'\n"
+    return p
+  #명령이 '주소'라면 모든 지역의 json주소를 출력
+  elif ar not in area.keys() : 
+    return "검색불가"
+  #존재하지 않은 지역이라면 "검색불가" 반환 후 함수 탈출
+  else :
+    rs = urlopen(url+area[ar]).read().decode('utf-8')
+    #url주소를 읽어 내용을 'utf-8'형식으로 디코딩하여 문자열 반환ㅌ
+    json_data = json.loads(rs)
+    #반환한 문자열을 json형식으로 읽음
+    info = json_data.get("getAirQualityInfoClassifiedByStation").get("item")[0]
+    #필요한 정보부분만 따로 추출
+
+    controlnumber = dating(info.get("controlnumber"))
+    #측정시간
+    pm10 = info.get("pm10")
+    #pm10
+    if info.get("pm10Cai") == '1' :
+      pm10Cai = "좋음" 
+    elif info.get("pm10Cai") == '2' : 
+      pm10Cai = "보통" 
+    elif info.get("pm10Cai") == '3' :
+      pm10Cai = "나쁨"
+    elif info.get("pm10Cai") == '4' :
+      pm10Cai = "매우나쁨"
+    else :
+      pm10Cai = "측정불가"
+    #pm10 지수
+    pm25 = info.get("pm25")
+    #pm25
+    if info.get("pm25Cai") == '1' :
+      pm25Cai = "좋음" 
+    elif info.get("pm25Cai") == '2' : 
+      pm25Cai = "보통" 
+    elif info.get("pm25Cai") == '3' :
+      pm25Cai = "나쁨"
+    elif info.get("pm25Cai") == '4' :
+      pm25Cai = "매우나쁨"
+    else :
+      pm25Cai = "측정불가"
+    #pm2.5 지수
+
+    return "측정 정보\n\n"+controlnumber+"에 측정됨\n\npm10 (미세먼지) : "+pm10+"μm/m³ ("+pm10Cai+")\npm2.5 (초미세먼지) : "+pm25+"μm/m³ ("+pm25Cai+")"
+
+
+
+def get_diet(code, ymd, weekday):
+    schMmealScCode = code #int 1조식2중식3석식
     schYmd = ymd #str 요청할 날짜 yyyy.mm.dd
+    if weekday == 5 or weekday == 6: #토요일,일요일 버림
+        element = " " #공백 반환
+    else:
+        num = weekday + 1 #int 요청할 날짜의 요일 0월1화2수3목4금5토6일 파싱한 데이터의 배열이 일요일부터 시작되므로 1을 더해줍니다.
+        URL = (
+                "http://stu.pen.go.kr/sts_sci_md01_001.do?"
+                "schulCode=C100000521&schulCrseScCode=4"
+                "&schulKndScCode=04"
+                "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
+            )
+
+        #기존 get_html 함수부분을 옮겨왔습니다.
+        html = ""
+        resp = requests.get(URL)
+        if resp.status_code == 200 : #사이트가 정상적으로 응답할 경우
+            html = resp.text
+        soup = BeautifulSoup(html, 'html.parser')
+        element_data = soup.find_all("tr")
+        element_data = element_data[2].find_all('td')
+        try:
+            element = str(element_data[num])
+
+            #filter
+            element_filter = ['[', ']', '<td class="textC last">', '<td class="textC">', '</td>', '&amp;', '(h)', '.']
+            for element_string in element_filter :
+                element = element.replace(element_string, '')
+            #줄 바꿈 처리
+            element = element.replace('<br/>', '\n')
+            #모든 공백 삭제
+            element = re.sub(r"\d", "", element)
+
+        #급식이 없을 경우
+        except:
+            element = " " # 공백 반환
+    return element
+
+def print_get_meal(local_date, local_weekday):
+        l_diet = get_diet(2, local_date, local_weekday)
+        d_diet = get_diet(3, local_date, local_weekday)
+
+        if len(l_diet) == 1:
+            result = "급식이 없습니다."
+            return result
+            
+        elif len(d_diet) == 1:
+            lunch = local_date + " 중식\n\n" + l_diet
+            return lunch
+        else:
+            lunch = local_date + " 중식\n\n" + l_diet + "\n"
+            dinner = local_date + " 석식\n\n" + d_diet
+            meal = lunch + dinner
+            return meal
+        
+def forecast() : #날씨 예보 파싱 함보
+  y=str(list(time.localtime(time.time()))[0])
+  m=str(list(time.localtime(time.time()))[1])
+  d=str(list(time.localtime(time.time()))[2])
+  h=str(list(time.localtime(time.time()))[3]+9)
+  mi=str(list(time.localtime(time.time()))[4])
+  
+  if int(h) >24 :
+    h = str(int(h-24))
+  if h != '2' and h != '5' and h!= '8' and h!= '11' and h != '14' and h != '17' and h != '20' and h != '23' :
+    a = int(h)%3
+    h=str(int(h)-a-1)
+
+    if int(mi)<15 :
+      h = int(h)-3
+
+    if h == '-1' :
+      h = '23'
+    elif h == '-4' :
+      h = '20'
+  if int(h)<10 :
+    h = '0'+h
+
+
+  key = 'ExhrDuBJZ28eMHPRIyFToDuqoT1Lx3ViPoI3uKVLS%2FyucnbaLbQISs4%2FSJWf0AzAV1gkbbtZK5GWvO9clF%2B1aQ%3D%3D'
+  #발급받은 인증키
+  url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?serviceKey='+key+'&base_date='+y+m+d+'&base_time='+h+'00&nx=99&ny=75&numOfRows=90&pageNo=1&_type=json'
+  #파싱할 json의 url
+
+  rs = urlopen(url).read().decode('utf-8')
+  #url주소를 읽어 내용을 'utf-8'형식으로 디코딩하여 문자열 반환
+  json_data = json.loads(rs)
+  #반환한 문자열을 json형식으로 읽음
+  info = json_data.get("response").get("body").get("items").get("item")
+  #필요한 정보부분만 따로 추출
+
+  ft = str(info[0].get("fcstTime"))
+  pop = ''
+  pty = ''
+  reh = ''
+  sky = ''
+  t3h = ''
+  tmn = ''
+  tmx = ''
+  vec = ''
+  wsd = ''
+  apm = ''
+  for i in range(len(info)) :
+    cate = info[i].get("category");
+    value = str(info[i].get("fcstValue"))
+    if str(info[i].get("fcstTime")) == ft :
+      if cate == 'POP' :
+        pop = value
+        #강수확률
+      elif cate == 'PTY' :
+        if value == '1' :
+          pty = ', 비'
+        elif value == '2' :
+          pty = ', 비/눈'
+        elif value == '3' :
+          pty = ', 눈/비'
+        elif value == '4' :
+          pty = ', 눈'
+        #강수형태
+      elif cate == 'REH' :
+        reh = value
+        #습도
+      elif cate == 'SKY' :
+        if value == '1' :
+          sky = '맑음'
+        elif value == '2' :
+          sky = '구름조금'
+        elif value == '3' :
+          sky = '구름많음'
+        elif value == '4' :
+          sky = '흐림'
+        #하늘상태
+      elif cate == 'T3H' :
+        t3h = value
+        #3시간 기온
+      elif cate == 'WSD' :
+        wsd = value
+        #풍속
+    if str(info[i].get("fcstTime")) == '1500' :
+      if cate == 'TMX' :
+        tmx = value
+    if str(info[i].get("fcstTime")) == '0600' :
+      if cate == 'TMN' :
+        tmn = value
+  if int(ft) < 1200 :
+    amp = '오전'
+  else :
+    amp = '오후'
+    ft = str(int(ft)-1200)
+    if int(ft) < 1000 :
+      ft = '0'+ft
+  p = amp+' '+ft[:2]+':'+ft[2:]+'\n\n최고 : '+tmx+'°C  최저 : '+tmn+'°C\n'+sky+pty+'\n현재온도 : '+t3h+'°C\n습도 : '+reh+'%\n강수확률 : '+pop+'%\n풍속 : '+wsd+'m/s'
+ 
+  return p
+
+def repl(self) :
+  a = self.replace(' ', '')
+  return a
+
+def call(msg) : 
+  call = {'교무실' :'702-0692',
+          '행정실' : '702-0691',
+          '교무실팩스' : '702-0686', 
+          '행정실팩스' : '702-0694', 
+          '교무기획부' : '709-7621', 
+          '교육과정부' : '709-7631', 
+          '생활안전부' : '709-7650', 
+          '교육정보부' : '709-7650', 
+          '방과후교육부' : '709-7645', 
+          '진로교육부' : '709-7642', 
+          '인문사회부' : '709-7656', 
+          '자연과학부' : '709-7690', 
+          '예체능교육부' : '709-7615', 
+          '3학년교육부' : '709-7660', 
+          '2학년교육부' : '709-7623', 
+          '1학년교육부' : '709-7685', 
+          '행정실민원' : '7097605', 
+          '보건실' : '709-7653', 
+          '도서실' : '709-7687', 
+          '급식실' : '709-7608', '위클래스' : '709-7658'}
+
+  msg = str(msg)
+  k = repl(msg)
+  rand = random.random()
+  if k in call.keys() :
+    return k+'의 내선번호 : '+call[k]
+  elif k == '강태경' and int(rand*100) == 0 :
+    return '개발자의 이름'
+  else :
+    return '검색불가'
+
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+    }
+    return jsonify(dataSend)
+
+@app.route('/dust', methods=['POST'])
+def dust():
+    d = get_micro('좌동')
+    print(d)
+    result = {
+        "version": "2.0",
+        "data": {
+            "dust": d
+        }
+    }
+    return jsonify(result)
+
+@app.route('/number', methods=['POST'])
+def number():
+    d = get_micro('좌동')
+    print(d)
+    result = {
+        "version": "2.0",
+        "data": {
+            "number": d
+            
+            def repl(self) :
+  a = self.replace(' ', '')
+  return a
+            
+            
+        }
+    }
+    return jsonify(result)
+
+@app.route('/weather', methods=['POST'])
+def weather():
+    weather = forecast()
+    print(weather)
+    result = {
+        "version": "2.0",
+        "data": {
+            "weather": weather
+        }
+    }
+    return jsonify(result)
+            
+@app.route('/meal', methods=['POST'])
+def meal():
+    request_body = request.get_json()  # request body 받음
+    params = request_body['action']['params'] # action > params로 파라미터에 접근
+    print(params)
+    date1 = json.loads(params['date'])
+    date1 = date1['date']
+    date2 = str(date1).replace("-", ".")
+
+    s = date2.replace('.', ', ') # 2017, 11, 21
+
+        #한자리수 달인 경우를 해결하기위함
+    if int(s[6:8]) < 10:
+        s = s.replace(s[6:8], s[7:8])
+
+    ss = "datetime.datetime(" + s + ").weekday()"
+    try:
+        whatday = eval(ss)
+    except:
+        
+        result = {
+            "version": "2.0",
+            "data": {
+                "meal": "급식이 없습니다."
+            }
+        }
+        return jsonify(result)
+    
+    print(str(date1)+"\n"+str(whatday))
+    meal = print_get_meal(date2, whatday)
+    print(meal)
+    result = {
+        "version": "2.0",
+        "data": {
+            "meal": meal
+        }
+    }
+    
+    return jsonify(result)
+
+@app.route('/schedule', methods=['POST'])
+def schedule():
+    today = datetime.date.today()
+
+    try:
+        officeCode="stu.pen.go.kr" ## 교육청 코드
+        schulCode="C100000521" ## 학교 고유코드
+        schulCrseCode="4" ## 학교 분류코드 (고등학교, 중학교, 초등학교)
+        schulKndScCode="0" + "4" ## 학교 분류코드
+        ay=today.year
+        mm=today.month
+    except:
+        print("""!!ERROR.... 
+                코드 인자 값이 맞지 않거나 값이 이상합니다.
+                제대로 된 코드 값을 입력해주십쇼""")
+        sys.exit(1)
+
+    ##neis web requests
+    URL="https://" + officeCode + "/sts_sci_sf01_001.do"
+    ##params = {'schulCode': 'B100000593', 'schulCrseScCode': '4', 'schulKndScCode' : '04', 'ay' : str(year)} 
+    params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 'schulKndScCode' : str(schulKndScCode), 'ay' : str(ay), 'mm' : str(mm)}
+    response = requests.get(URL, params=params).text
+    data = response[response.find("<tbody>"):response.find("</tbody>")]
+
+    ## re 정규표현식으로 불필요한 데이터를 자르거나 변환
+    regex = re.compile(r'[\n\r\t]')
+    data=regex.sub('',data)
+    rex = re.compile(r'<div class="textL">(.*?)</div>', re.S|re.M)
+    data=rex.findall(data)
+    element = ""
+
+    for dat in data:
+        date=dat[dat.find("<em>"):dat.find("</em>")][4:]
+        alert=dat[dat.find("<strong>"):dat.find("</strong>")][8:]
+        if date == "":
+            continue
+        if alert == "":
+            continue
+    
+        element = element+date+": "+alert+"\n"
+
+    print(element)
+    
+    result = {
+        "version": "2.0",
+        "data": {
+            "schedule": element
+        }
+    }
+    
+    return jsonify(result)
+
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000, threaded=True)
+
     if weekday == 5 or weekday == 6: #토요일,일요일 버림
         element = " " #공백 반환
     else:
