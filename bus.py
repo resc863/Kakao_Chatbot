20c3e344c0b837af890e23028a92c6f9dff7be75
resc863 <50689611+resc863@users.noreply.github.com>
2019-12-26 16:52:34 +0900

    Update bus.py

--- bus.py
+++ bus.py
@@ -1,21 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-def sexy():
-    sexy1="186190402"
-    sexy2="186210101"
-    url1 = 'http://61.43.246.153/openapi-data/service/busanBIMS2/stopArr?serviceKey=ExhrDuBJZ28eMHPRIyFToDuqoT1Lx3ViPoI3uKVLS%2FyucnbaLbQISs4%2FSJWf0AzAV1gkbbtZK5GWvO9clF%2B1aQ%3D%3D&bstopid='+sexy1
-    url2 = 'http://61.43.246.153/openapi-data/service/busanBIMS2/stopArr?serviceKey=ExhrDuBJZ28eMHPRIyFToDuqoT1Lx3ViPoI3uKVLS%2FyucnbaLbQISs4%2FSJWf0AzAV1gkbbtZK5GWvO9clF%2B1aQ%3D%3D&bstopid='+sexy2
+def bus():
+    result = "양운고 앞 대림1차아파트 정보\n\n"
+    bus1="186190402"
+    bus2="186210101"
+    url1 = 'http://61.43.246.153/openapi-data/service/busanBIMS2/stopArr?serviceKey=ExhrDuBJZ28eMHPRIyFToDuqoT1Lx3ViPoI3uKVLS%2FyucnbaLbQISs4%2FSJWf0AzAV1gkbbtZK5GWvO9clF%2B1aQ%3D%3D&bstopid='+bus1
+    url2 = 'http://61.43.246.153/openapi-data/service/busanBIMS2/stopArr?serviceKey=ExhrDuBJZ28eMHPRIyFToDuqoT1Lx3ViPoI3uKVLS%2FyucnbaLbQISs4%2FSJWf0AzAV1gkbbtZK5GWvO9clF%2B1aQ%3D%3D&bstopid='+bus2
     
     html = requests.get(url1).text
     soup = BeautifulSoup(html, "html.parser")
     item=soup.findAll('item')
-    for sex in item:
-        print(sex.lineno.string)
-    for sex1 in item:
-        if (sex1.min1.string==None):
-            print("현재 최근버스시간이 존재하지않습니다.")
+    for b in item:
+        result = result + b.lineno.string
+    for b1 in item:
+        if (b1.min1.string==None):
+            result = result + "현재 최근버스시간이 존재하지않습니다."
         else:
-            print(sex1.min1.string)
+            result = result + sex1.min1.string
         
-sexy()
+bus()
