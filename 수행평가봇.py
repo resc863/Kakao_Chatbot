7cefa3ebb604d86146384c3e795316b7c188e8d5
myeongju02 <58250777+myeongju02@users.noreply.github.com>
2019-12-27 17:10:07 +0900

    Add files via upload

---
+++ 수행평가봇.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+con = sqlite3.connect('2019.db')
+
+cur=con.cursor()
+
+
+print("몇월 수행평가를 출력하시겠습니까?")
+
+
+x=input()
+cur.execute("select * from Suhang where month="+ x)
+for row in cur:
+    print("과목 : " + str(row[4]))
+    print("이름 : " + str(row[3])) 
+    print("날짜 : " + str(row[0]) + " - " + str(row[1]) + " - " + str(row[2]))
+    print(" ")
+
