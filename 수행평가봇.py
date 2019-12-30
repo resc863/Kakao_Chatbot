d9399261a751984ae2c3b70a6ed96d375d8a55a5
resc863 <50689611+resc863@users.noreply.github.com>
2019-12-30 14:40:20 +0900

    Update 수행평가봇.py

--- 수행평가봇.py
+++ 수행평가봇.py
@@ -18,7 +18,7 @@ def result ():
     for row in cur:
         a = a+ ("과목 : " + str(row[4]) +'\n' +
              "내용 : " + str(row[3]) + '\n' +
-             "날짜 : " + str(row[0]) + " - " + str(row[1]) + " - " + str(row[2]) +'\n' +
+             "날짜 : " + str(row[0]) + "년 " + str(row[1]) + "월 " + str(row[2]) +'일\n' +
              "\n")
     return a
         
