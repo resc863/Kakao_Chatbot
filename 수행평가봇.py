d9327774830c8bbdc829f4b29251f1cdddee4366
myeongju02 <58250777+myeongju02@users.noreply.github.com>
2019-12-30 14:31:17 +0900

    Add files via upload

--- 수행평가봇.py
+++ 수행평가봇.py
@@ -10,9 +10,14 @@ print("몇월 수행평가를 출력하시겠습니까?")
 
 x=input()
 cur.execute("select * from Suhang where month="+ x)
-for row in cur:
-    print("과목 : " + str(row[4]))
-    print("이름 : " + str(row[3])) 
-    print("날짜 : " + str(row[0]) + " - " + str(row[1]) + " - " + str(row[2]))
-    print(" ")
 
+
+def result ():
+    for row in cur:
+        a = ("과목 : " + str(row[4]) +'\n' +
+             "이름 : " + str(row[3]) + '\n' +
+             "날짜 : " + str(row[0]) + " - " + str(row[1]) + " - " + str(row[2]) +'\n' +
+             " ")
+    return a
+        
+result()
