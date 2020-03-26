8ab8dd8a7b6d34d47b4133da508269fdb047a0a5
legendpartyG <58213474+legendpartyG@users.noreply.github.com>
2020-03-26 20:49:01 +0900

    Add files via upload

---
+++ 수행평가 전체검색.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+con = sqlite3.connect('2019.db')
+
+cur=con.cursor()
+
+
+print("몇학년 정보를 원하십니까?") #학년 선택 버튼
+
+
+x=input() # 2020 입력 함
+cur.execute("select * from Suhang where year="+ x)#전체 정보 출력을 위해 년도로 선택 
+
+
+
+def result ():   #정리 함수
+    a=""
+    for row in cur:
+        a = a+ ("과목 : " + str(row[3]) +'\n' +
+             "내용 : " + str(row[4]) + '\n' +
+             "날짜 : " + str(row[0]) + "년 " + str(row[1]) + "월 " + str(row[2]) +'일\n' +
+             "\n")
+    return a
+        
+print(result())
+
+
