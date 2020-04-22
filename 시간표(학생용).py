c72b1f6204ce468b8ec426bd75365a3d799e9424
resc863 <50689611+resc863@users.noreply.github.com>
2020-04-22 20:30:27 +0900

    Update 시간표(학생용).py

--- 시간표(학생용).py
+++ 시간표(학생용).py
@@ -1,50 +1,24 @@
 import sqlite3
 
- 
-
 conn = sqlite3.connect("timetable.db")
-
- 
-
 cur = conn.cursor()
 
- 
-
-
-
 def check():
-    finds=int(input("반을 입력하세요:"))
-    if finds==1:
-        finds='one'
-    elif finds==2:
-        finds='two'
-    elif finds==3:
-        finds='three'
-    elif finds==4:
-        finds='four'
-    elif finds==5:
-        finds='five'
-    elif finds==6:
-        finds='six'
-    elif finds==7:
-        finds='seven'
-    elif finds==8:
-        finds='eight'
-    elif finds==9:
-        finds='nine'
-    elif finds==10:
-        finds='ten'
+    finds=str(input("반을 입력하세요:"))
     return finds
 
 
 def result(finds):
-    cur.execute('select * from %s'%finds)
+    cur.execute("select * from '"+finds+"'")
 
 finds=check()    
 result(finds)
+r = ""
 
 for row in cur:
 
-    print(row)
+    r = r + str(row) + "\n"
+
+print(r)
     
 
