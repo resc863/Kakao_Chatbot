670622191804f45016f5f9ada62553491a33c604
resc863 <50689611+resc863@users.noreply.github.com>
2020-05-24 11:44:05 +0900

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -277,23 +277,21 @@ def dayCal() :
 
 
 def get_diet(code, ymd, weekday):
-    schMmealScCode = code #int 1조식2중식3석식
-    schYmd = ymd #str 요청할 날짜 yyyy.mm.dd
-    if weekday == 5 or weekday == 6: #토요일,일요일 버림
+    schMmealScCode = code #1 조식 2 중식 3 석식
+    schYmd = ymd #요청할 날짜
+    if weekday == 5 or weekday == 6: #토요일,일요일
         element = " " #공백 반환
     else:
-        num = weekday + 1 #int 요청할 날짜의 요일 0월1화2수3목4금5토6일 파싱한 데이터의 배열이 일요일부터 시작되므로 1을 더해줍니다.
+        num = weekday + 1 
         URL = (
                 "http://stu.pen.go.kr/sts_sci_md01_001.do?"
                 "schulCode=C100000521&schulCrseScCode=4"
                 "&schulKndScCode=04"
                 "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
             )
-
-        #기존 get_html 함수부분을 옮겨왔습니다.
         html = ""
         resp = requests.get(URL)
-        if resp.status_code == 200 : #사이트가 정상적으로 응답할 경우
+        if resp.status_code == 200 :
             html = resp.text
         soup = BeautifulSoup(html, 'html.parser')
         element_data = soup.find_all("tr")
@@ -301,18 +299,18 @@ def get_diet(code, ymd, weekday):
         try:
             element = str(element_data[num])
 
-            #filter
+            
             element_filter = ['[', ']', '<td class="textC last">', '<td class="textC">', '</td>', '&amp;', '(h)', '.']
             for element_string in element_filter :
                 element = element.replace(element_string, '')
             #줄 바꿈 처리
             element = element.replace('<br/>', '\n')
-            #모든 공백 삭제
+          
             element = re.sub(r"\d", "", element)
 
         #급식이 없을 경우
         except:
-            element = " " # 공백 반환
+            element = " " 
     return element
 
 def print_get_meal(local_date, local_weekday):
@@ -610,8 +608,7 @@ def db():
 
     content = body['contexts'][0]['params']
     detail = body['action']['detailParams']
-    print(content)
-    print(detail)
+    
 
     try:
         grade = content['학년']['resolvedValue'] #학년
@@ -758,12 +755,25 @@ def timetable_s():
     body = request.get_json()
     print(body)
 
-    #grade = body['action']['params']['학년']
+    grade = body['action']['params']['학년']
     finds = body['action']['params']['반']
-    conn = sqlite3.connect("timetable.db")
-    cur = conn.cursor()
+
+    if grade == "1":
+        conn = sqlite3.connect("timetable.db")
+        cur = conn.cursor()
     
-    cur.execute("select * from '"+finds+"'")
+        cur.execute("select * from '"+finds+"'")
+    elif grade == "2":
+        conn = sqlite3.connect("timetable2.db")
+        cur = conn.cursor()
+    
+        cur.execute("select * from '"+finds+"'")
+    else:
+        conn = sqlite3.connect("timetable3.db")
+        cur = conn.cursor()
+    
+        cur.execute("select * from '"+finds+"'")
+    
 
     r = ""
 
