bf41cf387da1f9507b6e4ac2841a16d1a35bf49b
resc863 <50689611+resc863@users.noreply.github.com>
2020-08-08 13:39:26 +0900

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -1,5 +1,51 @@
-from Chatbot import meal, db, weather, timetable_s
+import random, sqlite3
 
-def test_meal:
-    t = meal()
-    assert t != None
+def timetable_s():
+    body =  {
+                'action' :  {
+                    'params': {
+                        '학년' : str(random.randrange(1, 4)),
+                        '반' : str(random.randrange(1, 11))
+                    }
+                }
+            }
+
+    grade = body['action']['params']['학년']
+    finds = body['action']['params']['반']
+
+    if grade == "1":
+        conn = sqlite3.connect("timetable1.db")
+        cur = conn.cursor()
+    
+        cur.execute("select * from '"+finds+"'")
+    elif grade == "2":
+        conn = sqlite3.connect("timetable2.db")
+        cur = conn.cursor()
+    
+        cur.execute("select * from '"+finds+"'")
+    else:
+        conn = sqlite3.connect("timetable3.db")
+        cur = conn.cursor()
+    
+        cur.execute("select * from '"+finds+"'")
+    
+
+    a = '       월    화    수    목     금          \n'
+
+    for row in cur:
+        a = a + '   ' + str(row[0])+'   '+ str(row[1]) +'   '+str(row[2]) +'   '+str(row[3]) +'   '+str(row[4])+'   '+str(row[5] + "\n")
+        a =  a + "\n"
+
+    print(a)
+    
+    result = {
+        "version": "2.0",
+        "data": {
+            "timetable": a
+        }
+    }
+    return result
+
+def test_timetable():
+    t_db = timetable_s()
+    assert t_db != None
