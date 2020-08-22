369179f1537b7b2ca201ee20c56b6b573253e55b
resc863 <resc863@gmail.com>
2020-08-22 11:29:59 +0900

    cnt final

--- Chatbot.py
+++ Chatbot.py
@@ -101,8 +101,12 @@ def cnt2():
         month="0"+str(month)
 
     fanta=str(year)+str(month)+str(day)
-    url="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=JGMlPMEcTuNV8sbu5JRfjhwjPXMdCv1OJ1qQefm0vVuKWGKtGHAcJEWtm63GOVyMQYAcI%2BoXUBe0nsJ4w3RiZw%3D%3D&pageNo=1&numOfRows=10&startCreateDt="+fanta+"&endCreateDt="+fanta #call back url
-    test_url="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=JGMlPMEcTuNV8sbu5JRfjhwjPXMdCv1OJ1qQefm0vVuKWGKtGHAcJEWtm63GOVyMQYAcI%2BoXUBe0nsJ4w3RiZw%3D%3D&pageNo=1&numOfRows=10&startCreateDt=20200821&endCreateDt=20200821"
+    if (day == 1):
+        fanta1 = str(year)+str(month)+"28"
+    else:
+        fanta1 = str(year)+str(month)+str(int(day)-1)
+    url="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=0XeO7nbthbiRoMUkYGGah20%2BfXizwc0A6BfjrkL6qhh2%2Fsl8j9PzfSLGKnqR%2F1v%2F%2B6AunxntpLfoB3Ryd3OInQ%3D%3D&pageNo=1&numOfRows=10&startCreateDt="+fanta1+"&endCreateDt="+fanta #call back url
+    test_url="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=0XeO7nbthbiRoMUkYGGah20%2BfXizwc0A6BfjrkL6qhh2%2Fsl8j9PzfSLGKnqR%2F1v%2F%2B6AunxntpLfoB3Ryd3OInQ%3D%3D&pageNo=1&numOfRows=10&startCreateDt=20200821&endCreateDt=20200822"
 
     cola=requests.get(url).text
     sida=BeautifulSoup(cola, "html.parser")
@@ -111,27 +115,23 @@ def cnt2():
 
     for item in items :
         try:
-            hapgae=item.find("gubun").string
+            decidecnt=item.find("decidecnt").string
+            result[0]=decidecnt
         except:
-            continue
-        if hapgae == "합계" :
-            try:
-                defcnt=item.find("defcnt").string
-                result[0]=defcnt
-            except:
-                pass
-        if hapgae == "부산" :
-            try:
-                defcnt=item.find("defcnt").string
-                result[1]=defcnt
-            except:
-                pass
-        if hapgae == "합계" :
-            try:
-                deathcnt=item.find("deathcnt").string
-                result[2]=deathcnt
-            except:
-                pass
+            pass
+        
+        try:
+            clearcnt=item.find("clearcnt").string
+            result[1]=clearcnt
+        except:
+            pass
+
+        try:
+            deathcnt=item.find("deathcnt").string
+            result[2]=deathcnt
+        except:
+            pass
+        
     
     return result
 
