0194a6f4014ab3c9a1c840baabc5c8eac1827ebc
resc863 <50689611+resc863@users.noreply.github.com>
2020-12-13 11:50:41 +0900

    meal

---
+++ meal_test.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import urllib
+import requests, re
+import json
+import datetime
+from datetime import date
+
+today = str(date.today())
+today = today.replace("-", "")
+print(today)
+
+url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=4ff568a5dd3d4b3e918eb4d1478096d7&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150115&MLSV_YMD=20190508"
+js = requests.get(url).text
+
+data = json.loads(js)
+
+print(len(data))
+
+if ('mealServiceDietInfo' in data) == False:
+    print(data['RESULT']['MESSAGE'])
+
+else:
+    for i in range (len(data['mealServiceDietInfo'][1]['row'])):
+        if (data['mealServiceDietInfo'][1]['row'][i]['MMEAL_SC_CODE'] == '2') :
+            lunch = data['mealServiceDietInfo'][1]['row'][i]['DDISH_NM']
+            lunch = lunch.replace("<br/>", "\n")
+            print("lunch:\n"+lunch)
+    
+        elif (data['mealServiceDietInfo'][1]['row'][i]['MMEAL_SC_CODE'] == '3'):
+            dinner = data['mealServiceDietInfo'][1]['row'][i]['DDISH_NM']
+            dinner = dinner.replace("<br/>", "\n")
+            print("dinner:\n"+dinner)
+
